09c4970a218f0a6a9d7f5ff522b95744b2091099
eunsolkang <eunsol2953@gmail.com>
2022-02-21 14:37:52 +0900

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,25 @@
 import GameInitState from "./gameState/gameInitState";
 import GameReadyState from "./gameState/gameReadyState";
+import GameStartState from "./gameState/gameStartState";
+import InputSystem from "./system/inputSystem";
+import Setting from "./system/setting";
 
 export default class App{
     private canvas: HTMLCanvasElement;
     private ctx: CanvasRenderingContext2D;
     private gameState: GameState;
+    private setting: Setting;
 
     constructor(){
         this.canvas = <HTMLCanvasElement>document.getElementById('canvas');
         this.ctx = <CanvasRenderingContext2D>this.canvas.getContext('2d');
         this.gameState = new GameInitState();
+        this.setting = new Setting(this.canvas);
     }
 
     init(): void{
-        this.gameState = new GameReadyState(this.ctx);
+        this.setting.init();
+        this.gameState = new GameStartState(this.ctx, this.setting);
         this.gameLoop();
     }
 
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import App from './app';
+import InputSystem from './system/inputSystem';
 
 window.onload = () => {
+    new InputSystem();
     const app = new App();
     app.init();
 }
